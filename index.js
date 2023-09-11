eaf36c540ece9c9b8f2de33ad95234d567bfc29f
elina <papadelina145@gmail.com>
2023-09-11 10:27:50 +0300

    Implement page routing

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const express = require('express');
+
+const app = express();
+
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    res.sendFile('./views/index.html', {root: __dirname});
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', {root: __dirname});
+})
+
+app.get('/contact-me', (req, res) => {
+    res.sendFile('./views/contact-me.html', {root: __dirname});
+})
+
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', {root: __dirname});
+})
